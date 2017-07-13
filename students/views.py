2ad0fc874730b19171216e6a6a825aed1fdc9c91
Andrii Kobryk <a.kobryk@gmail.com>
2017-07-14 00:50:50 +0300

    add students list to the  main page

--- students/views.py
+++ students/views.py
@@ -9,7 +9,24 @@ from django.template import RequestContext, loader
 # Views for Students
 
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+		{'id': 1,
+		 'first_name': u'Андрій',
+		 'last_name': u'Кобрик',
+		 'ticket': 100,
+		 'image': 'img/me.jpg'},
+		{'id': 2,
+		 'first_name': u'Міла',
+		 'last_name': u'Спілберг',
+		 'ticket': 101,
+		 'image': 'img/1.jpeg'},
+		{'id': 3,
+		 'first_name': u'Арнольд',
+		 'last_name': u'Шварц',
+		 'ticket': 102,
+		 'image': 'img/2.jpeg'},
+		)
+	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
 	return HttpResponse('<h1>Students Add Form</h1>')
