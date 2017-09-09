46efbbc7f98229b65692f74070191421ddb96b9b
Andrii Kobryk <a.kobryk@gmail.com>
2017-09-09 11:39:41 +0300

    change db to MySQL

--- studentsdb/db.py
+++ studentsdb/db.py
@@ -5,19 +5,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-	'NAME': 'students_db',
+    #'default': {
+       # 'ENGINE': 'django.db.backends.postgresql',
+	#'NAME': 'students_db',
+	#'USER': 'students_db_user',
+	#'PASSWORD': '300629qweR',
+	#'HOST': 'localhost',
+	#'PORT': '',
+     
+'default': {
+	'ENGINE': 'django.db.backends.mysql',
+	'HOST': 'localhost',
 	'USER': 'students_db_user',
 	'PASSWORD': '300629qweR',
-	'HOST': 'localhost',
-	'PORT': '',
-        
+	'NAME': 'students_db',   
 
 
-#'HOST': 'localhost',
-        #'USER': 'students_db_user',
-       # 'PASSWORD': '300629qweR',
+	#'HOST': 'localhost',
+       # 'USER': 'students_db_user',
+        #'PASSWORD': '300629qweR',
         #'NAME': 'students_db',
     }
 }
