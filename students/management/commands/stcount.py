afd47d8f8c6a16f0659ac56bc065ed074951b646
Andrii Kobryk <a.kobryk@gmail.com>
2017-09-25 00:25:03 +0300

    create fill_db command

--- students/management/commands/stcount.py
+++ students/management/commands/stcount.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from students.models.students import Student
 from students.models.groups import Group
 from django.contrib.auth.models import User
+from students.models.exams import Exam
 
 class Command(BaseCommand):
 	help = 'Prints to console number of student related objects in a database'
@@ -10,9 +11,9 @@ class Command(BaseCommand):
 		parser.add_argument('student')
 		parser.add_argument('group')
 		parser.add_argument('user')
-
+		parser.add_argument('exam')
 	
-	models = (('student', Student), ('group', Group), ('user', User))
+	models = (('student', Student), ('group', Group), ('user', User), ('exam', Exam))
 
 	def handle(self, *args, **options):
 		for name, model in self.models:
